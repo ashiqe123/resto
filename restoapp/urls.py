4b03adeb81524b2f2af75c68cbec96dd73ea7247
ashiqe123 <muhammedashiqva@gmail.com>
2023-06-12 18:41:11 +0530

    second

--- restoapp/urls.py
+++ restoapp/urls.py
@@ -2,4 +2,9 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('', views.index,name='index'),
+    path('cat',views.cat,name='cat'),
+    path('about',views.about,name='about'),
+     path('cart',views.cart,name='cart'),
+     path('lg',views.lg,name='lg'),
+    path('home',views.home,name='home'),
 ]
--- restoapp/views.py
+++ restoapp/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def cat(request):
+    return render(request, 'category.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def lg(request):
+    return render(request, 'reg.html')
+
+def home(request):
+    return render(request,'home.html')
